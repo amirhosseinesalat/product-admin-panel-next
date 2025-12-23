18c269ac740499ece98e3ec101e646dccfcc5d72
amirhossein esalatfar <sltamirhosein@gmail.com>
2025-12-23 09:13:28 +0330

    update toast and debug

--- frontend/pages/_app.js
+++ frontend/pages/_app.js
@@ -1,7 +1,11 @@
-import '../styles/globals.css'
-
+import "../styles/globals.css";
+import { Toaster } from "react-hot-toast";
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Component {...pageProps} /> <Toaster position="top-center" />
+    </>
+  );
 }
 
-export default MyApp
+export default MyApp;
